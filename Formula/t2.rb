e39942983cc3857f1a39a30a174b055f6038d6bf
franpfeiffer <franciscopfeiffer.business@gmail.com>
2024-12-02 02:08:27 +0000

    no entiendo x1000

--- Formula/t2.rb
+++ Formula/t2.rb
@@ -4,7 +4,7 @@ class T2 < Formula
     url "https://github.com/franpfeiffer/t2/releases/tag/0.1.0/t2-linux-amd64" 
 
     version "0.1.0"
-    sha256 "2321c291093612f525576bf9002038377f46e690058b3f39923a64bc27972aef"
+    sha256 "36bcc1eb46fe07caa8621e4e338808b16f8a04edbfd72438bd351edace2faa24"
 
     def install
       bin.install "t2-linux-amd64" => "t2"
